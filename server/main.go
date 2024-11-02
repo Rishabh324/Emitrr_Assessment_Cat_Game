2f61f2c807ef63ddeb777e32a9cba7095bff8588
Rishabh324 <sinharishabh402@gmail.com>
2024-11-02 12:13:39 +0530

    Done with the changes

--- server/main.go
+++ server/main.go
@@ -12,7 +12,7 @@ import (
 
 func enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
